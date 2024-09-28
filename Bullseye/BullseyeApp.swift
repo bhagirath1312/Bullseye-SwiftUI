a2554f6828fcb287d2affd370fe12525a7e512c1
Bhagirath Bhatti <bhattibhagirath4618@gmail.com>
2024-09-28 19:57:33 +0530

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Bhagirath Bhatti on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Bhagirath Bhatti on 28/09/24.
+//
+
+import Testing
+@testable import Bullseye
+
+struct BullseyeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
