801ab528df477ed173b121d0aab2c968df7c3822
Bhagirath Bhatti <bhattibhagirath4618@gmail.com>
2024-09-28 20:57:00 +0530

    Cretae Project and add some UI

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,15 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("🎯🎯🎯\nPUT THE BULLESEYE AS CLONE AS YOU CAN TO")
+            Text("89")
+            HStack{
+                Text("0")
+                Slider(value:.constant(50), in: 1.0...100)
+                Text("100")
+
+            }
+            Button("Hit Me") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+           
         }
-        .padding()
+       
     }
 }
 
-#Preview {
-    ContentView()
+
+struct ContentView_Previews: PreviewProvider{
+    static var previews: some View{
+        ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+    }
 }
